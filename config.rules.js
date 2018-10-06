fdc09786bb73c6041dd06c8a253f5a0bedf4539d
rbrandt <rossmbrandt@gmail.com>
2018-10-05 23:06:33 -0500

    memoization

--- config.rules.js
+++ config.rules.js
@@ -1,4 +1,5 @@
 require("./src/helpers/_strings")
+const memoize = require("./src/helpers/_wrappers")
 
 const jsRules = (jsLoader) => {
 	return {
@@ -30,6 +31,12 @@ const configRules = ({
 			"{0}:\n    {yw~1:7}loader:  {2}\n    {yw~3:7}loader:  {4}".fmt(
 			caller, ".js", jsLoader, ".scss", styleLoader
 		))
+		function printAdd(num) { return "number given: "+num}
+		let ffff = memoize(printAdd)
+		let n = 0
+		while(n++ < 6){
+			console.log("{cy~0}".fmt(ffff(Math.floor(Math.random()*3)+1)))
+		}
 		return [ jsRules(jsLoader), scssRules(styleLoader) ]
 	} else {
 		console.log(
--- src/helpers/_wrappers.js
+++ src/helpers/_wrappers.js
@@ -1,4 +1,44 @@
-function gen(func, structure) {
+function memoize(func) {
+	if(func === undefined)
+		return undefined
+
+	function hash() {
+	}
+
+	let memory = {
+		memoized: new Map(),
+		run: function() {
+			let args = Array.prototype.slice.call(arguments).join()
+			//if(true)//memory.memoized.has(args))
+			//	return "IT'S WORKING:: "//+memory.memoized.get(args)
+
+			//console.log("{rd~0:9}:{1:_5}".fmt("mem size", memory.memoized.size))
+			
+			let response = func.apply(this, arguments)
+			console.log("{rd~0} : {1} : {2}".fmt(
+			args, response, memory.memoized.has(args)))
+			memory.memoized.set(args, response)
+			return response
+		}
+	}
+
+	return function() {
+		/*memory.memoized.forEach((value, key) => {
+			console.log(
+				"{ma~0}:{1}".fmt(
+				`memoized[${key}]`, value)
+			)
+		})*/
+		let args = Array.prototype.slice.call(arguments)
+		let str
+		args.forEach((e) => str+)
+		return (memory.memoized.has(args)) ?
+			memory.memoized.get(args) :
+			memory.run.apply(this, arguments)
+	}
+}
+
+function generator(func, structure) {
 	// structure of input matches structure output 
 	if(arguments[1] === undefined || arguments[1] === "match")
 		return function() {
@@ -42,4 +82,4 @@ function gen(func, structure) {
 	return undefined
 }
 
-module.exports = gen
+module.exports = memoize
