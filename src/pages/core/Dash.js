09d364917ac50666aec3abf07d785d8adb1267df
rbrandt <rossmbrandt@gmail.com>
2018-10-23 16:13:25 -0500

    colors fixed

--- src/pages/core/Dash.js
+++ src/pages/core/Dash.js
@@ -8,41 +8,47 @@ class Dash extends Component {
 				<aside className="dash">
 					<img src={"./assets/logo.svg"} alt="logo" className="logo" />
 					<div className="group">
-						Navigation
+						<div className="heading">
+							Navigation
+						</div>
+
+						<li>
+							<NavLink to="/home">
+								<i className="fas fa-home"></i>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/projects">
+								<i className="fas fa-project-diagram"></i>
+								Projects
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/notfound">
+								<i className="far fa-file-alt"></i>
+								Documents
+							</NavLink>
+						</li>
 					</div>
-					<li>
-						<NavLink to="/home">
-							<i className="fas fa-home"></i>
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/projects">
-							<i className="fas fa-project-diagram"></i>
-							Projects
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/notfound">
-							<i className="far fa-file-alt"></i>
-							Documents
-						</NavLink>
-					</li>
 					<div className="group">
-						Links
+						<div className="heading">
+							Links
+						</div>
+
+						<li>
+							<a href="https://www.linkedin.com/in/ross-brandt-1082a3b6">
+								<i className="fab fa-linkedin"></i>
+								LinkedIn
+							</a>
+						</li>
+						<li>
+							<a href="https://github.com/SomethingKL">
+								<i className="fab fa-github"></i>
+								GitHub
+							</a>
+						</li>
 					</div>
-					<li>
-						<a href="https://www.linkedin.com/in/ross-brandt-1082a3b6">
-							<i className="fab fa-linkedin"></i>
-							LinkedIn
-						</a>
-					</li>
-					<li>
-						<a href="https://github.com/SomethingKL">
-							<i className="fab fa-github"></i>
-							GitHub
-						</a>
-					</li>
 				</aside>
 				
 				{this.props.children}
